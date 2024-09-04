5df1db1c0521b57ed21c55f3ac95fe4e3406e056
SofiaMarcano <sofia.marcano@udea.edu.co>
2024-09-03 22:11:10 -0500

    Update paciente.py

--- paciente.py
+++ paciente.py
@@ -1,27 +1,28 @@
-paciente=[]
 pacientes=[]
-i=0
 numero_pac=int(input("Ingrese numero de pacientes a agregar: "))
 for i in range(numero_pac):
+    paciente=[]
     print(f"Paciente {i+1}")
     paciente.append(input("Nombre: "))
     paciente.append(int(input("Numero de identificacion: ")))
     paciente.append(input("Edad: "))
     paciente.append(input("Eps: "))
+    pacientes.append(paciente)
 
-pacientes.append(paciente)
-for paciente in pacientes:
-    if i==numero_pac:
-        salir=input("¿Quieres salir? (SI /NO)").strip().upper()
-        if salir=="SI":
-            break
-        elif salir=="NO":
-            numero_pac=input("Nuevo numero de pacientes: ")
-
-print("Lista de pacientes:")
-for idx, paciente in enumerate(pacientes,start=1):
-    print(f"Paciente {idx}")
-    print(f"Nombre: {paciente[0]}")
-    print(f"Identificacion: {paciente[1]}")
-    print(f"Edad:{paciente[2]}")
-    print(f"Eps: {paciente[3]}")
+for _ in range(numero_pac):
+    salir=input("¿Quieres salir? (SI /NO)").strip().upper()
+    if salir=="SI":
+        break
+    elif salir=="NO":
+        numero_pac=int(input("Nuevo numero de pacientes: "))
+        for j in range(numero_pac):
+            print(f"Paciente {len(pacientes)+j}")
+            paciente.append(input("Nombre: "))
+            paciente.append(int(input("Numero de identificacion: ")))
+            paciente.append(input("Edad:"))
+            paciente.append(input("Eps:"))   
+            pacientes.append(paciente)   
+             
+    else:
+        print("Responda correctamente")
+print(pacientes)

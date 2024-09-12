60b685885dd07b9bdfcd99305ed1d76153b6f436
SofiaMarcano <sofia.marcano@udea.edu.co>
2024-09-12 16:26:46 -0500

    Delete factorial.py

--- factorial.py
+++
@@ -1,9 +0,0 @@
-n=int(input("Ingresa un numero entre 0 y 20: "))
-fa=1
-i=1
-if n<=0 or n>20:
-    print("El numero tiene que estar entre 0 y 20")
-else:
-    for i in range(1,n+1):
-        fa *=i
-    print(f"El factorial de {n} es {fa}")

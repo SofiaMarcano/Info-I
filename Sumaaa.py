960a49246afbfd4b58f01d1a3e3e5d3ead9acdeb
SofiaMarcano <sofia.marcano@udea.edu.co>
2024-09-12 16:27:49 -0500

    Delete Sumaaa.py

--- Sumaaa.py
+++
@@ -1,8 +0,0 @@
-a=int(input("Ingrese el multiplicador:"))
-b=int(input("Ingrese el multiplicando"))
-cont=0
-res=0
-while cont<b:
-    res+=a
-    cont+=1
-print(f"La multiplicación de {a} y {b} por sumas es: {res}")

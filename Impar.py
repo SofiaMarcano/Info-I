08004104c977020079adb8d02cad99092a800a35
SofiaMarcano <sofia.marcano@udea.edu.co>
2024-09-12 16:27:04 -0500

    Delete Impar.py

--- Impar.py
+++
@@ -1,7 +0,0 @@
-N=int(input("Ingresa un numero: "))
-
-suma=0
-for i in range(N):
-    if i%2!=0:
-        suma+=i
-print(f"El resultado de de la suma numeros impares comprendidos de 0 a {N} es {suma}")
